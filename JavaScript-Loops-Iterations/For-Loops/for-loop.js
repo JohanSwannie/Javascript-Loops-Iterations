f1d7f5b355e74f7f7397aecbecf282d9694f2882
jswan <jswannieman@gmail.com>
2023-02-03 08:38:26 +1100

    Update for-loop HTML Component

--- JavaScript-Loops-Iterations/For-Loops/for-loop.js
+++ JavaScript-Loops-Iterations/For-Loops/for-loop.js
@@ -1,4 +1,4 @@
-// EXAMPLE 1
+// EXAMPLE 1 - For Loop
 
 const array1 = ["BMW", "Mercedes", "Audi"];
 
@@ -12,7 +12,7 @@ for (let i = 0; i < array1.length; i++) {
 
 document.getElementById("for-loop1").innerHTML = statement;
 
-// EXAMPLE 2
+// EXAMPLE 2 - For loop
 
 const array3 = [12, 17, 8, 22, 18, 14, 29, 4, 6];
 
@@ -31,3 +31,48 @@ average = (addition / array3.length).toFixed(2);
 concatString = `Sum of Array is ${addition} </br>Multiplication of Array is ${multiplication} </br>Average of Array is ${average}`;
 
 document.getElementById("for-loop2").innerHTML = concatString;
+
+// EXAMPLE 1 - For of loop
+
+const array4 = [17, 33, 10, 21, 28];
+
+let sumOfAll = 0;
+
+for (const value of array4) {
+  sumOfAll += value;
+}
+
+document.getElementById(
+  "for-of-loop"
+).innerHTML = `The sum of the values in array4 is ${sumOfAll}`;
+
+// EXAMPLE 1 For in loop
+
+const array5 = [
+  {
+    name: "John",
+    age: 55,
+    career: "Civil Engineer",
+  },
+  {
+    name: "Pete",
+    age: 45,
+    career: "Network Engineer",
+  },
+  {
+    name: "Mary",
+    age: 56,
+    career: "Software Engineer",
+  },
+];
+
+let personString = "";
+
+for (const key in array5) {
+  if (array5.hasOwnProperty.call(array5, key)) {
+    const element = array5[key];
+    personString += `${element.name} is ${element.age} years old and is a ${element.career} </br>`;
+  }
+}
+
+document.getElementById("for-in-loop").innerHTML = personString;
